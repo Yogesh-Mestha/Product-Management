dc712a6f393f7d900ff55c860ca95d561c42db86
Yogesh-Mestha <yogeshmestha80@gmail.com>
2024-08-13 20:38:04 +0530

    Delete beststore directory

--- beststore/src/main/java/com/boostmytool/beststore/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boostmytool.beststore;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Product, Integer> {
-    
-
-}
